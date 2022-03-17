022822bcdb1d815a1112577c62286e933f00016f
NiBbLe <noah.let4@gmail.com>
2022-03-17 12:03:46 -0700

    Updated the Dream Sheet and made all pages include reference page as part of heading box

--- js/DreamScript.js
+++ js/DreamScript.js
@@ -12,27 +12,54 @@ document.querySelector("body")
 // WIP
 function calculatePools() {
 
+	// add up the positive stats
 	var posStatTotal = 
 		document.getElementById("ambition").valueAsNumber
 		+ document.getElementById("creativity").valueAsNumber
 		+ document.getElementById("ire").valueAsNumber
 		+ document.getElementById("curiosity").valueAsNumber
 
+	// add up the negation stats
 	var negStatTotal = 
 		document.getElementById("defiance").valueAsNumber
 		+ document.getElementById("severity").valueAsNumber
 		+ document.getElementById("perseverance").valueAsNumber
 		+ document.getElementById("reservation").valueAsNumber
 
+	// calculate the offset toward positive or negation
 	var statTotal = negStatTotal+posStatTotal;
 	var multiplier = document.getElementById("actValue").valueAsNumber * 0.05;
 	var offset = statTotal * multiplier;
 	console.log("offset: "+offset);
+
+	// update the stat totals so they are the final max pool values
 	posStatTotal += offset;
 	negStatTotal -= offset;
 
 
+	// put the final max pool values into the greyed out boxes
 	document.getElementById("dreamPoolMax").innerHTML = posStatTotal;
 	document.getElementById("sleepPoolMax").innerHTML = negStatTotal;
+
+	 // Set the Current Pool values to equal the max values
+	document.getElementById("dreamPool").setAttribute('value',0+posStatTotal);
+	document.getElementById("sleepPool").setAttribute('value',0+negStatTotal);
 }
 
+function loseDreamPoint() {
+
+	var dreamPool = document.getElementById("dreamPool").valueAsNumber;
+
+	console.log("We ran the lose Dream Point function...");
+
+	document.getElementById("dreamPool").setAttribute('value',-1+dreamPool);
+}
+
+function loseSleepPoint() {
+
+	var sleepPool = document.getElementById("sleepPool").valueAsNumber;
+
+	console.log("We ran the lose Sleep Point function...");
+
+	document.getElementById("sleepPool").setAttribute('value',-1+sleepPool);
+}
